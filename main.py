35492258501bbe027d500e05494d2eb82366fdbf
dasilv-h3 <k.dasilva@h3hitema.fr>
2026-02-12 15:48:59 +0100

    main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+def main():
+    print("1 - Nouvelle partie")
+    print("2 - Charger partie")
+    print("3 - Quitter")
+
+    choice = input("Votre choix : " )
+
+    if choice == "1":
+        return 0
+    elif choice == "2":
+        return 0
+    elif choice == "3":
+        return 0
+    return 0
+
+if __name__ == "__main__":
+    main()
